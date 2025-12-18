9ad3dda6a43a18c7b74c767939dd162029f723b6
Astroite <574718927@qq.com>
2025-12-18 15:21:44 +0800

    支持作为应用安装

---
+++ sw.js
@@ -0,0 +1,52 @@
+// [重要] 每次发布新版本，务必修改这里的版本号！
+const CACHE_NAME = 'mindflow-v1.1';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/lib/d3.v7.min.js',
+    './js/lib/localforage.min.js',
+    './js/lib/marked.min.js',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[SW] Caching assets:', CACHE_NAME);
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[SW] Removing old cache:', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// [新增] 监听跳过等待的消息
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
